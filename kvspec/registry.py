b41029bbecfab4465d31125a42476bf6470369bb
sasano8 <sasano8@LAPTOP-PCMR5DJL>
2023-11-18 11:06:30 +0900

    update

--- kvspec/registry.py
+++ kvspec/registry.py
@@ -1,4 +1,14 @@
 class Registry(dict):
+    def __init__(self):
+        super().__init__()
+        self["loaders"] = {}
+        self["dumpers"] = {}
+
+    def __getitem__(self, key):
+        if key not in self:
+            raise KeyError(f"{key} not found in the registry.")
+        return super().__getitem__(key)
+
     def loader(self, cls):
         self["loaders"][cls.__name__] = cls
         return cls
@@ -6,6 +16,5 @@ class Registry(dict):
     def dumper(self, cls):
         self["dumpers"][cls.__name__] = cls
         return cls
-
 
 builtins = Registry(loaders={}, dumpers={})
