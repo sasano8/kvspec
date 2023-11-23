14a1e36ab44af8b53637d871964e1399e2ca52ae
sasano8 <sasano8@LAPTOP-PCMR5DJL>
2023-11-24 04:36:00 +0900

    update

--- kvschema/serializers.py
+++ kvschema/serializers.py
@@ -1,47 +1,51 @@
+class GeoParquetSerializer:
+    @staticmethod
+    def dump(f, obj):
+        import geopandas as gpd
 
-def get_sample():
-    import geopandas as gpd
-    from shapely.geometry import Point, Polygon
+        if isinstance(obj, gpd.GeoDataFrame):
+            obj.to_parquet(f)
+            # obj.to_file(f, driver="GeoJSON")
 
-    # サンプルデータの作成
-    data = {
-        'City': ['Tokyo', 'New York', 'London'],
-        'Country': ['Japan', 'USA', 'UK'],
-        'Latitude': [35.6895, 40.7128, 51.5074],
-        'Longitude': [139.6917, -74.0060, -0.1278]
-    }
+    @staticmethod
+    def load(f):
+        import geopandas as gpd
 
-    # GeoDataFrameの作成
-    gdf = gpd.GeoDataFrame(data)
+        return gpd.read_parquet(f)
 
-    # 中心点ジオメトリーの追加
-    gdf['Center'] = gdf.apply(lambda row: Point(row['Longitude'], row['Latitude']), axis=1)
+    @staticmethod
+    def sample():
+        import geopandas as gpd
+        from shapely.geometry import Point, Polygon
 
-    # 架空の境界ジオメトリーの追加（例：正方形の境界）
-    gdf['Boundary'] = gdf.apply(lambda row: Polygon([(row['Longitude']-0.1, row['Latitude']-0.1), 
-                                                    (row['Longitude']+0.1, row['Latitude']-0.1),
-                                                    (row['Longitude']+0.1, row['Latitude']+0.1),
-                                                    (row['Longitude']-0.1, row['Latitude']+0.1)]), axis=1)
+        # サンプルデータの作成
+        data = {
+            "City": ["Tokyo", "New York", "London"],
+            "Country": ["Japan", "USA", "UK"],
+            "Latitude": [35.6895, 40.7128, 51.5074],
+            "Longitude": [139.6917, -74.0060, -0.1278],
+        }
 
-    # 主ジオメトリーを指定）
-    return gdf.set_geometry('Center')
- 
+        # GeoDataFrameの作成
+        gdf = gpd.GeoDataFrame(data)
 
-def geopandas_to_geoparquet(obj, dest):
-    import geopandas as gpd
-    
-    if not isinstance(obj, gpd.GeoDataFrame):
-        raise Exception("obj must be GeoDataFrame")
+        # 中心点ジオメトリーの追加
+        gdf["Center"] = gdf.apply(
+            lambda row: Point(row["Longitude"], row["Latitude"]), axis=1
+        )
 
-    obj.to_parquet(dest)
-    
-def geoparquet_to_geopandas(path: str):
-    import geopandas as gpd
-    
-    return gpd.read_parquet(path)
+        # 架空の境界ジオメトリーの追加（例：正方形の境界）
+        gdf["Boundary"] = gdf.apply(
+            lambda row: Polygon(
+                [
+                    (row["Longitude"] - 0.1, row["Latitude"] - 0.1),
+                    (row["Longitude"] + 0.1, row["Latitude"] - 0.1),
+                    (row["Longitude"] + 0.1, row["Latitude"] + 0.1),
+                    (row["Longitude"] - 0.1, row["Latitude"] + 0.1),
+                ]
+            ),
+            axis=1,
+        )
 
-def geopostgres_to_geopandas(obj):
-    raise NotImplementedError()
-
-def geopostgres_to_geoparquet(obj, dest):
-    raise NotImplementedError()
+        # 主ジオメトリーを指定
+        return gdf.set_geometry("Boundary")
--- tests/test_transfomer.py
+++ tests/test_transfomer.py
@@ -69,3 +69,129 @@ def test_timestamp():
     assert encoder.decode("str", BASE_FLOAT) == BASE_STR
     assert encoder.decode("isoformat", BASE_FLOAT) == BASE_STR
     assert encoder.decode("datetime", BASE_FLOAT) == BASE_DT
+
+
+def test_serializer():
+    import geopandas as gpd
+    from kvschema.serializers import GeoParquetSerializer
+    from shapely.geometry.base import BaseGeometry
+    from shapely import wkt
+
+    BASE_DIR = "test_storage/"
+
+    df1 = GeoParquetSerializer.sample()
+    GeoParquetSerializer.dump(BASE_DIR + "geo1.parquet", df1)
+    df2 = GeoParquetSerializer.load(BASE_DIR + "geo1.parquet")
+
+    row1 = df1.iloc[0]
+    row2 = df2.iloc[0]
+
+    columns = {
+        "City": str,
+        "Country": str,
+        "Latitude": float,
+        "Longitude": float,
+        "Center": BaseGeometry,
+        "Boundary": BaseGeometry,
+    }
+
+    assert len(df1.columns) == len(columns)
+
+    for col, typ in columns.items():
+        assert df1[col].equals(df2[col])
+        assert df1[col].dtype == df2[col].dtype
+        assert isinstance(df1.iloc[0][col], typ)
+        assert isinstance(df2.iloc[0][col], typ)
+
+    assert df1.columns.equals(df2.columns), "Column names mismatch"
+    assert df1.shape == df2.shape, "Rows and columns count mismatch"  # 行と列数の比較
+    assert df1.geometry.equals(df2.geometry), "Geometry data mismatch"
+    assert (df1.geometry == df2.geometry).all(), "Geometry data mismatch"
+    assert df1.crs == df2.crs, "CRS mismatch"
+
+    assert df1.geometry.name == "Boundary"
+    assert df2.geometry.name == "Boundary"
+
+    df3 = df2.set_geometry("Center")
+    assert df3.geometry.name == "Center"
+
+    GeoParquetSerializer.dump(BASE_DIR + "geo2.parquet", df3)
+    df4 = GeoParquetSerializer.load(BASE_DIR + "geo2.parquet")
+
+    assert df4.geometry.name == "Center"
+
+    # 対応ドライバの確認
+    if False:
+        # 一般的な地理空間IOライブラリ
+        import fiona
+
+        print(fiona.supported_drivers)
+        {
+            "DXF": "rw",
+            "CSV": "raw",
+            "OpenFileGDB": "raw",
+            "ESRIJSON": "r",
+            "ESRI Shapefile": "raw",
+            "FlatGeobuf": "raw",
+            "GeoJSON": "raw",
+            "GeoJSONSeq": "raw",
+            "GPKG": "raw",
+            "GML": "rw",
+            "OGR_GMT": "rw",
+            "GPX": "rw",
+            "MapInfo File": "raw",
+            "DGN": "raw",
+            "S57": "r",
+            "SQLite": "raw",
+            "TopoJSON": "r",
+        }
+
+    if False:
+        # fiona より、GeoPandas に特化し、最適化が図られたライブラリ
+        import pyogrio
+
+        pyogrio.list_drivers()
+
+    print(df4.info())
+    """
+    #   Column     Non-Null Count  Dtype   
+    ---  ------     --------------  -----   
+    0   City       3 non-null      object  
+    1   Country    3 non-null      object  
+    2   Latitude   3 non-null      float64 
+    3   Longitude  3 non-null      float64 
+    4   Center     3 non-null      geometry
+    5   Boundary   3 non-null      geometry
+    """
+
+    with pytest.raises(Exception):
+        # GeoJSONは１つのジオメトリーのみ保持できる
+        # 複数のジオメトリーを保持する場合は、wktを復元したり手動の対応が必要
+        df4.to_file(BASE_DIR + "geo3.json", driver="GeoJSON")
+
+    df4["Boundary"] = df4["Boundary"].to_wkt()
+    df4.to_file(BASE_DIR + "geo3.json", driver="GeoJSON")
+
+    if False:
+        # wkt列をgeometryに復元
+        df4["Boundary"] = gpd.GeoSeries.from_wkt(df4["Boundary"])
+        print(df4.info())
+        print(df4.head())
+
+        print("================")
+        # 列名が返る <class 'str'>
+        for row in df4:
+            print(type(row))
+            print(row)
+
+        print("================")
+        # NamedTupleのようなものが返る <class 'pandas.core.series.Series'>
+        for index, row in df4.iterrows():
+            print(type(row))
+            print(row)
+
+        print("================")
+        # <class 'pandas.core.frame.Pandas'>
+        for row in df4.itertuples():
+            print(type(row))
+            print(row.City)
